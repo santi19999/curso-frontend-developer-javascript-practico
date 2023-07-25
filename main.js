18d7458b94cb9d27b04a78c57a482457eaf7947c
santi herrera <santiherrera1999@gmail.com>
2023-07-24 23:09:42 -0300

    push

--- main.js
+++ main.js
@@ -3,43 +3,52 @@ const email = document.querySelector('.navbar-email');
 const menu = document.querySelector('.desktop-menu');
 const menuMobile = document.querySelector('.mobile-menu');
 const imgMenu = document.querySelector('.menu');
-const productDetail = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const shoppingCart = document.querySelector('.shopping-cart');
 const cardsContainer = document.querySelector('.cards-container');
-const fragment = document.createDocumentFragment();
+const productDetailClose = document.querySelector('.product-detail-close');
+const productDetail = document.querySelector('#productDetail');
+
 email.addEventListener('click', (e) => {
-	productDetail.classList.forEach((item) => {
+	shoppingCartContainer.classList.forEach((item) => {
 		if (item === 'inactive') {
 			return;
 		} else {
-			productDetail.classList.add('inactive');
+			shoppingCartContainer.classList.add('inactive');
 		}
 	});
 	menu.classList.toggle('inactive');
 });
 imgMenu.addEventListener('click', (e) => {
-	productDetail.classList.add('inactive');
+	shoppingCartContainer.classList.add('inactive');
 	menuMobile.classList.toggle('inactive');
 });
 shoppingCart.addEventListener('click', (e) => {
 	menu.classList.add('inactive');
 	menuMobile.classList.add('inactive');
-	productDetail.classList.toggle('inactive');
+	shoppingCartContainer.classList.toggle('inactive');
+	productDetail.classList.add('inactive');
 });
-
+productDetailClose.addEventListener('click', (e) => {
+	productDetail.classList.add('inactive');
+	shoppingCartContainer.classList.toggle('inactive');
+});
 productList.push({
+	id: 1,
 	name: 'Bike',
 	imagen:
 		'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
 	precio: 120,
 });
 productList.push({
+	id: 2,
 	name: 'Pantalla',
 	imagen:
 		'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2019/10/elige-mejor-monitor-cada-tipo-juego_1.jpg?tf=1200x',
 	precio: 320,
 });
 productList.push({
+	id: 3,
 	name: 'Escritorio',
 	imagen:
 		'https://img.freepik.com/foto-gratis/oficina-moderna-vista-ciudad_1340-37540.jpg?size=626&ext=jpg&ga=GA1.1.991922780.1687302473&semt=sph',
@@ -49,8 +58,10 @@ productList.push({
 function renderProducts(arr) {
 	for (product of arr) {
 		const productCard = document.createElement('div');
+		productCard.id = product.id;
 		productCard.classList.add('product-card');
 		const img = document.createElement('img');
+		img.classList.add('productImg');
 		img.setAttribute('src', product.imagen);
 		const productInfoDiv = document.createElement('div');
 		const productInfo = document.createElement('div');
@@ -58,9 +69,11 @@ function renderProducts(arr) {
 		productInfo.classList.add('product-info');
 
 		const productPrice = document.createElement('p');
+		productPrice.classList.add('priceProduct');
 
 		productPrice.innerText = '$' + product.precio;
 		const productName = document.createElement('p');
+		productName.classList.add('nameProduct');
 		productName.innerText = product.name;
 		productInfoDiv.appendChild(productPrice);
 		productInfoDiv.appendChild(productName);
@@ -80,3 +93,19 @@ function renderProducts(arr) {
 }
 
 renderProducts(productList);
+const productCard = document.querySelectorAll('.product-card');
+
+productCard.forEach((card) => {
+	card.addEventListener('click', (e) => {
+		let precio = document.querySelector('.price');
+		let nombre = document.querySelector('.name');
+		let image = document.querySelector('#image');
+		const urlImage = e.currentTarget.children[0].currentSrc;
+		let [name, price] = e.currentTarget.children[1].children[0].children;
+		nombre.textContent = name.textContent;
+		precio.textContent = price.textContent;
+		image.setAttribute('src', urlImage);
+
+		productDetail.classList.remove('inactive');
+	});
+});
